1763559808ea4d250c740f8a3f600d1e91536351
knucklessg1 <knucklessg1@gmail.com>
2026-05-27 23:54:00 -0500

    Bump version: 0.12.0 → 0.13.0

--- scholarx/agent_server.py
+++ scholarx/agent_server.py
@@ -25,7 +25,7 @@ warnings.filterwarnings("ignore", message=".*urllib3.*or charset_normalizer.*")
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="fastmcp")
 
 
-__version__ = "0.12.0"
+__version__ = "0.13.0"
 
 logging.basicConfig(
     level=logging.INFO,
